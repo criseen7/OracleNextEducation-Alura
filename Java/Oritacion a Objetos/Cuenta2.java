a2d3d054421e73fcd2537e1668c4033f74b93b92
Cristofer Raziel <criseen7@gmail.com>
2023-03-21 22:23:35 -0600

    Constructores y statics

--- Java/Oritacion a Objetos/Cuenta2.java
+++ Java/Oritacion a Objetos/Cuenta2.java
@@ -1,13 +1,13 @@
-//Entidad cuenta:
-class Cuenta {
+//Entidad cuenta 2:
+class Cuenta2 {
     private double saldo;
     private int agencia;
     private int numero;
     private Cliente titular;
     
-    /*private static int total=0; //dato static
+    private static int total=0; //dato static
     
-    public Cuenta(int agencia) {//constructor
+    public Cuenta2(int agencia) {//constructor
     	
     	if(agencia<=0)
     		this.agencia = 1;
@@ -15,7 +15,7 @@ class Cuenta {
     		this.agencia=agencia;
     	total++;
     	System.out.println("Se van creando: "+total+" cuentas");
-    }*/
+    }
     
     void depositar(double dinero){
     	this.saldo=this.saldo+dinero;
@@ -38,9 +38,7 @@ class Cuenta {
     public double getSaldo() {//getter
     	return saldo;
     }
-    public void setAgencia(int agencia) {
-		this.agencia = agencia;
-	}
+    
     public int getAgencia() {
 		return agencia;
 	}
@@ -56,5 +54,8 @@ class Cuenta {
 	}
     public Cliente getTitular() {
 		return titular;
+	}
+    public static int getTotal() {//Metodo static -> miembro estatico
+		return total;//al no ser un atributo, no puedo usar this
 	}
 }
---
+++ Java/Oritacion a Objetos/pruebaConstructor.java
@@ -0,0 +1,10 @@
+
+public class pruebaConstructor {
+	public static void main(String[] args) {
+		Cuenta2 cuenta = new Cuenta2(555);
+		Cuenta2 cuenta2 = new Cuenta2(333);
+		Cuenta2 cuenta3 = new Cuenta2(444);
+		System.out.println(Cuenta2.getTotal());//Solo porque es estatico
+		
+	}
+}
