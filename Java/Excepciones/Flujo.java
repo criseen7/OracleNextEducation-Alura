6a16adbdaa0d279b8257df05f62d492a05d1e3c8
Cristofer Raziel <criseen7@gmail.com>
2023-05-14 12:37:28 -0600

    Checked & Unchecked

--- Java/Excepciones/Flujo.java
+++ Java/Excepciones/Flujo.java
@@ -16,28 +16,9 @@ public class Flujo{
         System.out.println("Fin metodo1");
     }
 
-    private static void metodo2(){
+    private static void metodo2() throws MiException{//Throws si se lanza atrapalo
         System.out.println("Inicio metodo2");
-        //for(int i=1; i<=5; i++){
-            /*try{//intenta esto
-                if(i==3){
-                    int num=0;
-                    int resultado =1/num;
-                    System.out.println(resultado);
-                }
-                String test = null;
-                System.out.println(test.toString());
-            }catch(ArithmeticException | NullPointerException exception){//atrapa el error
-                System.out.println(exception.getMessage());
-                exception.printStackTrace();
-            }//try no puede existir sin catch*/
-           // System.out.println(i);
-       // }
-        /*ArithmeticException ae = new ArithmeticException();//creando mi propia excepcion
-        throw ae;//lanza la exception pero no deja compilar nada debajo de la instruccion
-        //System.out.println("Fin metodo2");*/
         throw new MiException("Mi excepcion fue lanzada");
-        
     }
 
 }
--- Java/Excepciones/MiException.java
+++ Java/Excepciones/MiException.java
@@ -1,9 +1,8 @@
-public class MiException extends RuntimeException{
-    /*public MiException{
-        super();
-    }*/
-
+public class MiException extends Exception{
+//RuntimeException -> unchecked 
+//Exception -> checked
     public MiException(String message) {
         super(message);
     }
+
 }
