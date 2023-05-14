3aaafb4d71ae83de2b4eb3c2a312a378d7deaa32
Cristofer Raziel <criseen7@gmail.com>
2023-05-14 12:04:42 -0600

    Errores y Excepciones

--- Java/Excepciones/Flujo.java
+++ Java/Excepciones/Flujo.java
@@ -8,14 +8,18 @@ public class Flujo{
     
     private static void metodo1(){
         System.out.println("Inicio metodo1");
-        metodo2();
+        try{
+            metodo2();
+        }catch(MiException me){
+            me.printStackTrace();
+        }
         System.out.println("Fin metodo1");
     }
 
     private static void metodo2(){
         System.out.println("Inicio metodo2");
-        for(int i=1; i<=5; i++){
-            try{//intenta esto
+        //for(int i=1; i<=5; i++){
+            /*try{//intenta esto
                 if(i==3){
                     int num=0;
                     int resultado =1/num;
@@ -26,10 +30,14 @@ public class Flujo{
             }catch(ArithmeticException | NullPointerException exception){//atrapa el error
                 System.out.println(exception.getMessage());
                 exception.printStackTrace();
-            }//try no puede existir sin catch
-            System.out.println(i);
-        }
-        System.out.println("Fin metodo2");
+            }//try no puede existir sin catch*/
+           // System.out.println(i);
+       // }
+        /*ArithmeticException ae = new ArithmeticException();//creando mi propia excepcion
+        throw ae;//lanza la exception pero no deja compilar nada debajo de la instruccion
+        //System.out.println("Fin metodo2");*/
+        throw new MiException("Mi excepcion fue lanzada");
+        
     }
 
 }
--- Java/Oritacion a Objetos/pruebaAcceso.java
+++
@@ -1,9 +0,0 @@
-
-public class pruebaAcceso {
-	public static void main(String[] args) {
-		Cuenta cuenta = new Cuenta();
-		cuenta.depositar(400);
-		cuenta.retirar(300);
-		System.out.println(cuenta.getSaldo());
-	}
-}
